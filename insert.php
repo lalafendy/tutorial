cc0fdec9964eca1f407bf808dca36835f91e665f
lalafendy <46051318+lalafendy@users.noreply.github.com>
2022-05-10 23:01:48 +0800

    Initial commit

---
+++ insert.php
@@ -0,0 +1,9 @@
+<?php  
+ //insert.php  
+ //$connect = mysqli_connect("localhost", "root", "", "testing");  
+ if(isset($_POST["name"]))  
+ { 
+     var_dump($_POST["name"]);
+     exit();
+ }  
+ ?>  
